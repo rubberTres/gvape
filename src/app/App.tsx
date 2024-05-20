50d5b03ccf77bc3d7c80436e1de611051e4117c5
Oskar Jaworski <oskar.jaworski@weastronauts.com>
2024-05-21 00:26:56 +0200

    [BMW] Goodbye Gvape

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,3 @@
-import Card from "app/components/Card";
-import { useEffect, useState } from "react";
-import { Taste } from "app/types";
-import { db } from "firebase.config";
-import { collection, getDocs } from "@firebase/firestore";
 import ReactGA from "react-ga4";
 
 ReactGA.initialize("G-RCKC26CWB5");
@@ -10,42 +5,18 @@ ReactGA.send(window.location.pathname);
 
 function App() {
 
-	const [ tastes, setTastes ] = useState<Taste[]>([]);
-
-	const [ selectedCardId, setSelectedCardId ] = useState<string | null>(null);
-
-	const tastesCollectionRef = collection(db, "tastes");
-
-	const getTastes = async () => {
-		const data = await getDocs(tastesCollectionRef);
-		const mappedData = data.docs.map(doc => ({ ...doc.data(), id: doc.id })) as unknown as Taste[];
-		setTastes(mappedData);
-	}
-
-	useEffect(() => {
-		getTastes();
-	}, []);
-
-
 	return (
 		<main className="w-screen">
 			<header className="h-14 shadow-md flex items-center pl-5 text-4xl">
 				GVAPE
 			</header>
-			<div className="grid gap-10 place-items-center p-5 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1">
-				{
-					tastes.map(taste =>
-						taste.count !== 0
-							&&
-							<Card
-								key={ taste.id }
-								taste={ taste }
-								selectedCardId={ selectedCardId }
-								setSelectedCardId={ setSelectedCardId }
-
-							/>
-					)
-				}
+			<div className="w-full flex justify-center items-center mt-10">
+				<img
+					src="https://pngimg.com/uploads/bmw_logo/bmw_logo_PNG19707.png"
+					alt="BMW"
+					className="w-20 h-20"
+				/>
+				<span className="text-3xl ml-4"> - <b className="text-sky-500">B</b>yło <b className="text-blue-500">M</b>iło <b className="text-red-500">W</b>ypierdalam</span>
 			</div>
 		</main>
 	);
